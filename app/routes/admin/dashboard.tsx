8d46fe461b25317d9ee4a83dffe437fb3358b9dd
saira512dev <512saira@gmail.com>
2025-05-28 23:43:47 +0530

    started with dashboard page

---
+++ app/routes/admin/dashboard.tsx
@@ -0,0 +1,94 @@
+import { NavItems, StatsCard, TripCard } from "@/components";
+import Header from "@/components/Header";
+
+type Props = {};
+
+const allTrips = [
+  {
+    id: 1,
+    name: "Tropical Rewind",
+    imageUrls: ["/assets/images/sample1.jpg"],
+    itinerary: [{ location: "Thailand" }],
+    tags: ["Adventure", "Culture"],
+    travelStyle: "Solo",
+    estimatedPrice: "$1,000",
+  },
+  {
+    id: 2,
+    name: "French Reverie",
+    imageUrls: ["/assets/images/sample2.jpg"],
+    itinerary: [{ location: "Paris" }],
+    tags: ["Relaxation", "Culinary"],
+    travelStyle: "Family",
+    estimatedPrice: "$2,000",
+  },
+  {
+    id: 3,
+    name: "Zen Break",
+    imageUrls: ["/assets/images/sample3.jpg"],
+    itinerary: [{ location: "Japan" }],
+    tags: ["Shopping", "Luxury"],
+    travelStyle: "Couple",
+    estimatedPrice: "$3,000",
+  },
+  {
+    id: 4,
+    name: "Adventure in Westeros",
+    imageUrls: ["/assets/images/sample4.jpg"],
+    itinerary: [{ location: "Croatia" }],
+    tags: ["Historical", "Culture"],
+    travelStyle: "Friends",
+    estimatedPrice: "$4,000",
+  },
+];
+
+const Dashboard = (props: Props) => {
+  const user = {
+    name: "Saira",
+  };
+
+  const dashboardStats = {
+    totalUsers: 12450,
+    usersJoined: { currentMonth: 218, lastMonth: 176 },
+    totalTrips: 3210,
+    tripsCreated: { currentMonth: 150, lastMonth: 250 },
+    userRole: { total: 62, currentMonth: 25, lastMonth: 15 },
+  };
+
+  const { totalUsers, usersJoined, totalTrips, tripsCreated, userRole } =
+    dashboardStats;
+
+  return (
+    <main className="dashboard wrapper">
+      <Header
+        title={`Welcome ${user?.name ?? "Guest"}`}
+        description="Track activity, trends and popular destination in real time"
+      />
+      <section className="flex flex-col gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full">
+          <StatsCard
+            headerTitle="total Users"
+            total={totalUsers}
+            currentMonthCount={usersJoined.currentMonth}
+            lastMonthCount={usersJoined.lastMonth}
+          />
+          <StatsCard
+            headerTitle="total Trips"
+            total={totalTrips}
+            currentMonthCount={tripsCreated.currentMonth}
+            lastMonthCount={tripsCreated.lastMonth}
+          />
+          <StatsCard
+            headerTitle="Active Users "
+            total={userRole.total}
+            currentMonthCount={userRole.currentMonth}
+            lastMonthCount={userRole.lastMonth}
+          />
+        </div>
+      </section>
+      <TripCard />
+    </main>
+  );
+};
+
+export default Dashboard;
