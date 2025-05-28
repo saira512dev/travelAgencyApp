c5413cfa04d27b39ab7dec67d25c1bc51a1b6600
saira512dev <512saira@gmail.com>
2025-05-28 23:43:35 +0530

    started with all users page

---
+++ app/routes/admin/all-users.tsx
@@ -0,0 +1,14 @@
+import { Header } from "@/components";
+
+type Props = {};
+
+const AllUsers = (props: Props) => {
+  return (
+    <main className="dashboard wrapper">
+      <Header title="Trips Page" description="Manage Users" />
+      Content
+    </main>
+  );
+};
+
+export default AllUsers;
